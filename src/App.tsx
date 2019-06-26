cbb994c2743bf41e26a44a404504a0dac03e77fd
Marcus Notheis <marcus.notheis@sap.com>
2019-06-26 19:22:16 +0200

    Updated Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
-import { ShellBar, ThemeProvider } from '@ui5/webcomponents-react';
+import { ShellBar, ThemeProvider, Text } from '@ui5/webcomponents-react';
 import '@ui5/webcomponents/dist/Button';
 import React, { createRef, PureComponent, RefObject } from 'react';
 import './App.css';
 
 class App extends PureComponent {
+  state = {
+    count: 0
+  };
+
   private btnRef: RefObject<HTMLElement> = createRef();
 
-  private buttonClickHandler = (e: any) => {
-    alert('Hello World!');
+  private onButtonClick = (e: any) => {
+    this.setState({
+      count: this.state.count + 1
+    });
   };
 
   componentDidMount() {
     if (this.btnRef.current) {
-      this.btnRef.current.addEventListener('press', this.buttonClickHandler);
+      this.btnRef.current.addEventListener('press', this.onButtonClick);
     }
   }
 
   render() {
     return (
       <ThemeProvider>
-        <ShellBar primaryTitle="UI5con Demo" secondaryTitle="UI5 Web Components for React" />
+        <ShellBar
+          primaryTitle="UI5con Demo"
+          secondaryTitle="UI5 Web Components for React"
+          logo="https://appletouchicons-eacp.dispatcher.hana.ondemand.com/SAP/Icon-60.png"
+          showCoPilot
+          showNotifications
+          showProductSwitch
+          notificationCount={`${this.state.count}`}
+        />
         <div className="App">
-          {/* 
-          // @ts-ignore */}
-          <ui5-button ref={this.btnRef}>Click me!</ui5-button>
+          <Text>Click Count: {this.state.count}</Text>
+          <br />
+          <ui5-button ref={this.btnRef}>
+            Click me!
+          </ui5-button>
         </div>
       </ThemeProvider>
     );
--- src/App_backup.tsx
+++ src/App_backup.tsx
@@ -1,25 +1,47 @@
-import React, { PureComponent, createRef, RefObject } from 'react';
-import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
-import { ShellBar } from '@ui5/webcomponents-react/lib/ShellBar';
-import { Button } from '@ui5/webcomponents-react/lib/Button';
-import { ButtonDesign } from '@ui5/webcomponents-react/lib/ButtonDesign';
+import { ShellBar, ThemeProvider, Text, Button } from '@ui5/webcomponents-react';
+import '@ui5/webcomponents/dist/Button';
+import React, { createRef, PureComponent, RefObject } from 'react';
 import './App.css';
 
 class App extends PureComponent {
+  state = {
+    count: 0
+  };
+
   private btnRef: RefObject<HTMLElement> = createRef();
 
-  private buttonClickHandler = (e: any) => {
-    alert('Hello World!');
+  private onButtonClick = (e: any) => {
+    this.setState({
+      count: this.state.count + 1
+    });
   };
+
+  componentDidMount() {
+    if (this.btnRef.current) {
+      this.btnRef.current.addEventListener('press', this.onButtonClick);
+    }
+  }
 
   render() {
     return (
       <ThemeProvider>
-        <ShellBar primaryTitle="UI5con Demo" secondaryTitle="UI5 Web Components for React" />
+        <ShellBar
+          primaryTitle="UI5con Demo"
+          secondaryTitle="UI5 Web Components for React"
+          logo="https://appletouchicons-eacp.dispatcher.hana.ondemand.com/SAP/Icon-60.png"
+          showCoPilot
+          showNotifications
+          showProductSwitch
+          notificationCount={`${this.state.count}`}
+        />
         <div className="App">
-          <Button onPress={this.buttonClickHandler} disabled={false} design={ButtonDesign.Accept}>
+          <Text>Click Count: {this.state.count}</Text>
+          <br />
+          <ui5-button ref={this.btnRef} disabled={false}>
             Click me!
-          </Button>
+          </ui5-button>
+          <br />
+          <Button onPress={this.onButtonClick}>React Click</Button>
         </div>
       </ThemeProvider>
     );
