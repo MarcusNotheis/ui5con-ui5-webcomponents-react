d4ec65b9d4c59515144562d90bdc8f006e4a98e0
Marcus Notheis <marcus.notheis@sap.com>
2019-06-27 14:34:48 +0200

    Change skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ class App extends PureComponent {
         <div className="App">
           <Text>Click Count: {this.state.count}</Text>
           <br />
-          <ui5-button ref={this.btnRef}>
+          <ui5-button press={this.onButtonPress}>
             Click UI5 Web Component
           </ui5-button>
           <br />
